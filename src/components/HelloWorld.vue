7dfd31bba1b8e31a915c8fa983d5bcbb2709d69c
tomptaylor <tomptaylor@tomptaylors-MacBook-Pro.local>
2021-11-20 18:14:56 -0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+ MultiSelect value: {{ multiselectValue }}
+    <br />
+    <br />
+    <kendo-multiselect v-model="multiselectValue"
+                        :data-source="dataSourceArray"
+                        :data-text-field="'text'"
+                        :data-value-field="'value'"
+                        :placeholder="'Select size...'"
+                        :filter="'contains'">
+    </kendo-multiselect>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { MultiSelect } from '@progress/kendo-dropdowns-vue-wrapper';
+//Vue.use(MultiSelect);
+
+export default defineComponent({
+   components: {
+        'kendo-multiselect': MultiSelect
+    },
+    data: function() {
+        return {
+            multiselectValue: "",
+            dataSourceArray: [
+                { text: 'Baseball', value: '1' },
+                { text: 'Basketball', value: '2' },
+                { text: 'Cricket', value: '3' },
+                { text: 'Football', value: '4' },
+                { text: 'Tennis', value: '5' },
+                { text: 'Volleyball', value: '6' }
+            ]
+        }
+    }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@progress/kendo-ui';
+import '@progress/kendo-theme-default';
+import { MultiSelect } from '@progress/kendo-dropdowns-vue-wrapper'
+
+createApp(App).mount('#app')
